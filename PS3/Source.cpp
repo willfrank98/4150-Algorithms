678d1d0a0bc610c55e6b3f0b0ed92cb499df38ff
Will Frank <willfrank98@gmail.com>
2018-02-06 10:44:20 -0700

    PS4/Source.cpp

---
+++ PS3/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double diameter;
+	cin >> diameter;
+
+	double stars;
+	cin >> stars;
+
+	for (int i = 0; i < stars; i++)
+	{
+		double x;
+		cin >> x;
+
+		double y;
+		cin >> y;
+	}
+}
