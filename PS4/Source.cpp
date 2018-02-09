a26a80148f2c8b27a21cf0011047a08e0db0da6b
Will Frank <willfrank98@gmail.com>
2018-02-08 22:50:12 -0700

    some stuff

--- PS4/Source.cpp
+++ PS4/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
 int main()
 {
-	
+	int cities;
+	cin >> cities;
+
+	map<string, map<string, int>> Map;
+	map<string, int> tolls;
+
+	for (int i = 0; i < cities; i++)
+	{
+		string city;
+		cin >> city;
+
+		int toll;
+		cin >> toll;
+
+		tolls[city] = toll;
+	}
+
+	int highways;
+	cin >> highways;
+
+	for (int i = 0; i < cities; i++)
+	{
+		string start;
+		cin >> start;
+
+		string end;
+		cin >> end;
+
+		Map[start][end] = tolls[end];
+	}
+
+
 }
