92f610c621fa9cb1e302650562947bd41562496e
Will Frank <willfrank98@gmail.com>
2018-02-09 22:21:41 -0700

    yarrrrrrrrrrrr

--- PS4/Source.cpp
+++ PS4/Source.cpp
@@ -6,12 +6,14 @@
 
 using namespace std;
 
+map<string, map<string, int>> Map;
+vector<string> sorted;
+
 int main()
 {
 	int cities;
 	cin >> cities;
 
-	map<string, map<string, int>> Map;
 	map<string, int> tolls;
 
 	for (int i = 0; i < cities; i++)
@@ -39,7 +41,8 @@ int main()
 		Map[start][end] = tolls[end];
 	}
 
-	topoSort();
+	
+	//sorted = topoSort();
 
 	int trips;
 	cin >> trips;
@@ -58,24 +61,40 @@ int main()
 		}
 		else
 		{
-			cout << findValue() << endl;
+			topoSort(start);
+
+			//reverseVec(sorted);
 		}
 	}
 
 
 }
 
-void topoSort()
+void topoSort(string startCity)
 {
-
+	sorted = vector<string>();
+	depthFirstSearch(startCity, 0);
 }
 
-void depthFirstSearch(string startCity)
+void depthFirstSearch(string startCity, int value)
 {
+	//this iterates through the map
+	for (map<string, int>::iterator it = Map[startCity].begin(); it != Map[startCity].end(); it++)
+	{
+		depthFirstSearch(it->first, value++);
+	}
 
+	sorted.push_back(startCity);
 }
 
-int findValue()
+vector<string> reverseVec(vector<string> vec)
 {
+	vector<string> temp(vec.size());
 
+	for (int i = 0; i < vec.size(); i++)
+	{
+		temp[i] = vec[vec.size() - i - 1];
+	}
+
+	return temp;
 }
