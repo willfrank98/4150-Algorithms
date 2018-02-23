2c538da3f800b4616d7b3ea875937258a6ef4527
Will Frank <willfrank98@gmail.com>
2018-02-23 14:14:14 -0700

    feel like this shouldn't have worked, but it did

--- PS6/Source.cpp
+++ PS6/Source.cpp
@@ -1,46 +1,57 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <queue>
-#include <map>
 
 using namespace std;
 
-map<int, map<int, double>> edges;
+double findPath(int v);
+
+vector<vector<pair<int, double>>> edges;
 
 int main()
 {
-	//loop it
-	int v;
-	cin >> v;
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(4); //sets output to 4 decimal places
 
-	int e;
-	cin >> e;
-
-	if (v == 0 && e == 0)
+	while (1 < 2) //is this bad practice?
 	{
-		return 0;
-	}
+		int v;
+		cin >> v;
 
-	edges = map<int, map<int, double>>();
-	for (int i = 0; i < e; i++)
-	{
-		int v1;
-		cin >> v1;
+		int e;
+		cin >> e;
 
-		int v2;
-		cin >> v2;
+		if (v == 0 && e == 0)
+		{
+			return 0;
+		}
 
-		double f;
-		cin >> f;
+		edges = vector<vector<pair<int, double>>>(v, vector<pair<int, double>>());
+		//vector<vector<pair<int, double>>> edges2(v, vector<pair<int, double>>(v));
+		
+		for (int i = 0; i < e; i++)
+		{
+			int v1;
+			cin >> v1;
 
-		edges[v1][v2] = f;
-		edges[v2][v1] = f;
+			int v2;
+			cin >> v2;
+
+			double f;
+			cin >> f;
+
+			//undirected edges
+			edges[v1].push_back(pair<int, double>(v2, f));
+			edges[v2].push_back(pair<int, double>(v1, f));
+		}
+
+		cout << findPath(v) << endl;
 	}
 	
-	findPath(v);
 }
 
-void findPath(int v)
+double findPath(int v)
 {
 	//setup
 	vector<double> size(v, 0);
@@ -49,23 +60,24 @@ void findPath(int v)
 	size[0] = 1;
 
 	priority_queue<pair<double, int>> pq;
-	pq.push(pair<double, int>(1, 0));
+	pq.push(pair<double, int>(1.0, 0));
 
 	while (!pq.empty())
 	{
 		int u = pq.top().second;
 		pq.pop();
 
-		for (auto& v : edges[u])
+		for (pair<int, double>& v : edges[u])
 		{
-			double w = edges[u][v.first];
+			double w = v.second;
 			if (size[v.first] < size[u] * w)
 			{
 				size[v.first] = size[u] * w;
 				prev[v.first] = u;
-				pq.push(v.first); //this won't really work
+				pq.push(pair<double, int>(size[v.first], v.first));
 			}
 		}
-
 	}
+
+	return size[v - 1];
 }
