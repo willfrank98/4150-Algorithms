30177851bf2532d5abdb7ca457757b248f177d11
Will Frank <willfrank98@gmail.com>
2018-03-06 11:16:57 -0700

    ps7

---
+++ PS7/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	while (!cin.eof())
+	{
+		string type;
+		cin >> type;
+
+		if (type == "gcd")
+		{
+
+		}
+		else if (type == "exp")
+		{
+
+		}
+		else if (type == "inverse")
+		{
+
+		}
+		else if (type == "isprime")
+		{
+
+		}
+		else if (type == "key")
+		{
+
+		}
+	}
+}
