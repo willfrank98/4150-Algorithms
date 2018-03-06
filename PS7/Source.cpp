49d33c476a80445cde8a7573e8a4affcdb7fa897
Will Frank <willfrank98@gmail.com>
2018-03-06 16:25:18 -0700

    commitment

--- PS7/Source.cpp
+++ PS7/Source.cpp
@@ -12,23 +12,66 @@ int main()
 
 		if (type == "gcd")
 		{
-
+			long a, b;
+			cin >> a >> b;
+			cout << gcd(a, b);
 		}
 		else if (type == "exp")
 		{
-
+			long x, y, N;
+			cin >> x >> y >> N;
+			cout << exp(x, y, N);
 		}
 		else if (type == "inverse")
 		{
-
+			long a, N;
+			cin >> a >> N;
+			cout << inverse(a, N);
 		}
 		else if (type == "isprime")
 		{
-
+			long p;
+			cin >> p;
+			cout << isprime(p);
 		}
 		else if (type == "key")
 		{
-
+			long p, q;
+			cin >> p >> q;
+			cout << key(p, q);
 		}
 	}
 }
+
+long gcd(long a, long b)
+{
+	if (b == 0)
+	{
+		return a;
+	}
+	else
+	{
+		return gcd(b, a%b);
+	}
+}
+
+long exp(long x, long y, long N)
+{
+	while 
+}
+
+long inverse(long a, long N)
+{
+
+}
+
+long isprime(long p)
+{
+
+}
+
+long key(long p, long q)
+{
+
+}
+
