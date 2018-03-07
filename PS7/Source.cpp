1a8aa93ed371f8fa98d89b3d4032be07875d6184
Will Frank <willfrank98@gmail.com>
2018-03-06 20:28:45 -0700

    yaw

--- PS7/Source.cpp
+++ PS7/Source.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+long gcd(long a, long b);
+long exp(long x, long y, long N);
+string inverse(long a, long N);
+long* ee(long a, long b);
+bool isprime(long p);
+void key(long p, long q);
+
 int main()
 {
 	while (!cin.eof())
@@ -32,46 +39,114 @@ int main()
 		{
 			long p;
 			cin >> p;
-			cout << isprime(p);
+			if (isprime(p))
+			{
+				cout << "yes";
+			}
+			else
+			{
+				cout << "no";
+			}
 		}
 		else if (type == "key")
 		{
 			long p, q;
 			cin >> p >> q;
-			cout << key(p, q);
+			key(p, q);
 		}
 	}
 }
 
 long gcd(long a, long b)
 {
-	if (b == 0)
+	while (b > 0)
 	{
-		return a;
+		long N = a % b;
+		a = b;
+		b = N;
 	}
-	else
-	{
-		return gcd(b, a%b);
-	}
+	return a;
 }
 
 long exp(long x, long y, long N)
 {
-	while 
+	if (y == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		long z = exp(x, y / 2, N);
+		if (y % 2 == 0)
+		{
+			return (z * z) % N;
+		}
+		else
+		{
+			long temp = (z * z) % N;
+			return (x * temp) % N;
+		}
+	}
 }
 
-long inverse(long a, long N)
+string inverse(long a, long N)
 {
+	long* arr = ee(a, N);
+	if (arr[2] == 1)
+	{
+		return "" + arr[0] % N;
+	}
+	else
+	{
+		return "none";
+	}
+}
+
+long* ee(long a, long b)
+{
+	long temp[3];
+	long* arr = temp;
+	if (b == 0)
+	{
+		arr[0] = 1;
+		arr[1] = 0;
+		arr[2] = a;
+		return arr;
+	}
+	else
+	{
+		arr = ee(b, a%b);
+		long x = arr[1];
+		long y = arr[0] - (a / b)*arr[1];
+		long z = arr[2];
+		arr[0] = x;
+		arr[1] = y;
+		arr[2] = z;
+		return arr;
+	}
+}
+
+bool isprime(long p)
+{
+	if (exp(2, p - 1, p) == 1 &&
+		exp(3, p - 1, p) == 1 &&
+		exp(5, p - 1, p) == 1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
 }
 
-long isprime(long p)
+void key(long p, long q)
 {
+	long N = p * q;
+	cout << N;
 
-}
-
-long key(long p, long q)
-{
+	long phi = (p - 1)*(q - 1);
 
 }
 
