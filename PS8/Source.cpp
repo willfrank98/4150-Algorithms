c74ccde863bf2dc07e51f158a3f25737f8a19986
Will Frank <willfrank98@gmail.com>
2018-03-14 00:00:16 -0600

    wrong

--- PS8/Source.cpp
+++ PS8/Source.cpp
@@ -1,71 +1,72 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+//#include <queue>
 
 using namespace std;
 
-class mypair
-{
-public:
-	int first;
-	int second;
-
-	mypair(int f, int s) : first(f), second(s)
-	{}
-
-	bool operator > (mypair p) const
-	{
-		if (first < p.first)
-		{
-			return true;
-		}
-		else if (first > p.first)
-		{
-			return false;
-		}
-		else
-		{
-			if (second > p.second)
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	bool operator < (mypair p) const
-	{
-		if (first > p.first)
-		{
-			return true;
-		}
-		else if (first < p.first)
-		{
-			return false;
-		}
-		else
-		{
-			if (second < p.second)
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	bool operator == (mypair p) const
-	{
-		if (first == p.first && second == p.second)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-};
+//class mypair
+//{
+//public:
+//	int first;
+//	int second;
+//
+//	mypair(int f, int s) : first(f), second(s)
+//	{}
+//
+//	bool operator < (mypair p) const
+//	{
+//		if (first < p.first)
+//		{
+//			return true;
+//		}
+//		else if (first > p.first)
+//		{
+//			return false;
+//		}
+//		else
+//		{
+//			if (second < p.second)
+//			{
+//				return true;
+//			}
+//		}
+//
+//		return false;
+//	}
+//
+//	bool operator > (mypair p) const
+//	{
+//		if (first > p.first)
+//		{
+//			return true;
+//		}
+//		else if (first < p.first)
+//		{
+//			return false;
+//		}
+//		else
+//		{
+//			if (second > p.second)
+//			{
+//				return true;
+//			}
+//		}
+//
+//		return false;
+//	}
+//
+//	bool operator == (mypair p) const
+//	{
+//		if (first == p.first && second == p.second)
+//		{
+//			return true;
+//		}
+//		else
+//		{
+//			return false;
+//		}
+//	}
+//};
 
 int main()
 {
@@ -75,17 +76,26 @@ int main()
 	int time;
 	cin >> time;
 
-	priority_queue<mypair> patrons;
+	//priority_queue<mypair> patrons;
+	vector<int> money(time, 0);
 	for (int i = 0; i < people; i++)
 	{
 		int c;
 		int t;
 		cin >> c >> t;
 
-		patrons.push(mypair(t, c));
+		//patrons.push(mypair(t, c));
 	}
 
-	int money = 0;
+	int total = 0;
+	for (int i = 0; i < money.size(); i++)
+	{
+		total += money[i];
+	}
+
+	cout << total;
+
+	/*int money = 0;
 	for (int i = 0; i < time; i++)
 	{
 		mypair temp = patrons.top();
@@ -99,7 +109,7 @@ int main()
 		{
 			patrons.pop();
 		}
-	}
+	}*/
 
-	cout << money;
+	//cout << money;
 }
